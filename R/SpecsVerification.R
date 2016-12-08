0fa64dfc27119c6870d73ab9635b229630f3d37c
Stefan Siegert <stefan_siegert@gmx.de>
2016-12-08 21:26:49 +0000

    added importfroms

--- R/SpecsVerification.R
+++ R/SpecsVerification.R
@@ -4,6 +4,11 @@
 #' @docType package
 #' @name SpecsVerification
 #' @importFrom Rcpp evalCpp
+#' @importFrom grDevices gray
+#' @importFrom graphics axis barplot box hist lines mtext par plot points polygon
+#' @importFrom stats coef cor cov dist dnorm lm optim pbinom pchisq pnorm pt qnorm quantile rbinom rnorm sd var
+#' @importFrom utils tail
+
 #' @useDynLib SpecsVerification
 
 NULL
