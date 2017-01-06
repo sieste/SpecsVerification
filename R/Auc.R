8b48e6eaffa8439c76059f2488cffdfbcea2cd8b
Stefan Siegert <stefan_siegert@gmx.de>
2017-01-06 12:36:09 +0000

    implemented Cpp/R switch in Auc function

--- R/Auc.R
+++ R/Auc.R
@@ -3,18 +3,20 @@
 #' @param fcst vector of forecasts
 #' @param obs vector of binary observations (0 for non-occurrence, 1 for occurrence of the event)
 #' @param handle.na how should missing values in forecasts and observations be handled; possible values are 'na.fail' and 'only.complete.pairs'; default: 'na.fail'
-#' @return vector containing AUC and its estimated standard deviation
+#' @param use_fn the function used for the calculation: 'C++' (default) for the fast C++ implementation, or 'R' for the slow (but more readable) R implementation 
+#' @return vector containing AUC and its estimated sampling standard deviation
 #' @examples
 #' data(eurotempforecast)
 #' Auc(rowMeans(ens.bin), obs.bin)
 #' @seealso AucDiff
 #' @references DeLong et al (1988): Comparing the Areas under Two or More Correlated Receiver Operating Characteristic Curves: A Nonparametric Approach. Biometrics. http://dx.doi.org/10.2307/2531595
 #' @export
-Auc <- function(fcst, obs, handle.na=c("na.fail", "only.complete.pairs")) {
+Auc = function(fcst, obs, handle.na=c("na.fail", "only.complete.pairs"), use_fn=c('C++','R')) {
 
 
   ## sanity checks
   stopifnot(length(fcst) == length(obs))
+
 
   ## handle NA's
   handle.na = match.arg(handle.na)
@@ -23,13 +25,14 @@ Auc <- function(fcst, obs, handle.na=c("na.fail", "only.complete.pairs")) {
       stop("missing values")
     }
   } else if (handle.na == "only.complete.pairs") {
-    nna <- !is.na(fcst) & !is.na(obs)
+    nna = !is.na(fcst) & !is.na(obs)
     if (all(nna == FALSE)) {
       stop("there are no complete sets of forecasts and observations")
     }
-    fcst <- fcst[nna]
-    obs <- obs[nna]
+    fcst = fcst[nna]
+    obs = obs[nna]
   } 
+
 
   ## after removing any NA's, check if observations are either 0 or 1
   stopifnot(all(obs %in% c(0,1)))
@@ -40,37 +43,49 @@ Auc <- function(fcst, obs, handle.na=c("na.fail", "only.complete.pairs")) {
     stop("need at least one event and one non-event")
   }
 
-
-  ## calculate sets of forecasts with events (X) and forecasts with non-events (Y)
-  X <- fcst[obs == 1]
-  Y <- fcst[obs == 0]
-
-
-  ## Delong's Psi function as matrix (Psi.mat[i, j] = Psi(X[i], Y[j]))
-  psi.fun <- function(x, y) {
-    return(1 * (x > y) + 0.5 * (x == y))
-  }
-  Psi <- outer(X, Y, psi.fun)
-
   
-  ## AUC estimates
-  theta <- mean(Psi)
+  # decide whether to use the fast C function or the slow but more readable R function
+  use_fn = match.arg(use_fn)
+  if (use_fn == 'C++') {
 
 
-  ## Delong's S matrix and variance estimate
-  V <- rowMeans(Psi)
-  W <- colMeans(Psi)
+    ans = auc_cpp(fcst, obs)
+    auc = ans[1]
+    auc_sd = ans[2]
 
-  v <- sum((V - theta)^2) / (n1 - 1)
-  w <- sum((W - theta)^2) / (n0 - 1)
 
-  var.auc <- v / n1 + w / n0
+  } else {
+  
 
-  sd.auc <- sqrt(var.auc)
+    ## calculate sets of forecasts with events (X) and forecasts with non-events (Y)
+    X = fcst[obs == 1]
+    Y = fcst[obs == 0]
+  
+  
+    ## Delong's Psi function as matrix (Psi_mat[i, j] = Psi(X[i], Y[j]))
+    psi_fun = function(x, y) {
+      return(1 * (x > y) + 0.5 * (x == y))
+    }
+    Psi = outer(X, Y, psi_fun)
+  
+    
+    ## AUC estimates
+    auc = mean(Psi)
+  
+  
+    ## Delong et al (1988) variance estimate
+    V = rowMeans(Psi)
+    W = colMeans(Psi)
+    v = var(V)
+    w = var(W)
+    auc_sd = sqrt(v / n1 + w / n0)
+  
+
+  }
 
 
   ## return
-  ret <- c(auc = theta, auc.sd = sd.auc)
+  ret = c(auc = auc, auc_sd = auc_sd)
   return(ret)
 }
 
