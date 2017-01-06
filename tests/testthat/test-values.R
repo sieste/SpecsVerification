1d48da7dbeab61cf5f9ba385c55d18685a5447db
Stefan Siegert <stefan_siegert@gmx.de>
2017-01-06 13:35:09 +0000

    added crps tests

--- tests/testthat/test-values.R
+++ tests/testthat/test-values.R
@@ -10,6 +10,8 @@ test_that('EnsCrps returns correct values', {
   expect_equal(EnsCrps(matrix(1), 2, R.new=Inf), NA_real_)
   expect_equal(EnsCrps(matrix(1:3, 1, 3), 2, R.new=0), NA_real_)
   expect_equal(EnsCrps(matrix(c(1:5, NA), 2, 3, TRUE), c(1.5, 4.5)), c(5/6-4/9, 1/2-1/4))
+  expect_equal(EnsCrps(matrix(c(NA,NA), 1, 2), 1), NA_real_)
+  expect_equal(EnsCrps(matrix(1:2, 1, 2), NA), NA_real_)
 })
 
 
