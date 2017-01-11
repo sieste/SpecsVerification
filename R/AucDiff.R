3938d0c91b5a3dbe86750d3c69f56af141d55c11
Stefan Siegert <stefan_siegert@gmx.de>
2017-01-11 14:57:38 +0000

    added sun/xu reference to auc functions

--- R/AucDiff.R
+++ R/AucDiff.R
@@ -11,6 +11,7 @@
 #' AucDiff(rowMeans(ens.bin), ens.bin[, 1], obs.bin)
 #' @seealso Auc
 #' @references DeLong et al (1988): Comparing the Areas under Two or More Correlated Receiver Operating Characteristic Curves: A Nonparametric Approach. Biometrics. http://dx.doi.org/10.2307/2531595
+#' Sun and Xu (2014): Fast Implementation of DeLong's Algorithm for Comparing the Areas Under Correlated Receiver Operating Characteristic Curves. IEEE Sign Proc Let 21(11). http://dx.doi.org/10.1109/LSP.2014.2337313
 #' @export
 AucDiff <- function(fcst, fcst.ref, obs, handle.na=c('na.fail', 'only.complete.triplets'), 
                     use_fn=c('C++','R')) {
